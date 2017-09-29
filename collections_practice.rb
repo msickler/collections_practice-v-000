a34d73ae72fd85dc472a5ada43bb15db7534731e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 12:43:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,19 @@
 def sort_array_asc(array)
   array.sort
 end
+
+def sort_array_desc(array)
+  array.sort |a, b|
+  if a == b
+    0
+  elsif a < b 
+    1
+  elsif a > b
+    -1
+  end   
+  end
+    
+    
+    
+  end
+end
