55eeed17430b686a2f4123805bdbb0f9c21eeb88
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 13:17:24 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif a > b
+    -1
+  end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |letter|
+    letter[2] = "$"
+    end
+end
+
+def find_a(array)
+  array.find_all { |letter| letter.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, a| sum + a }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |a, index|
+    if index != 1
+      a << "s"
+    else index = 1
+      a
+    end
+  end
+end
